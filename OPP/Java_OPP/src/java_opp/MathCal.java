d6bcf7b87c548ca38b6d0d7f621d4e2912585ac6
SefaKabatas <81758761+SefaKabatas@users.noreply.github.com>
2021-09-10 16:17:38 +0300

    OPP

---
+++ OPP/Java_OPP/src/java_opp/MathCal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_opp;
+
+/**
+ *
+ * @author sefa_
+ */
+public class MathCal {
+    //Static Poliymorphism
+    public int sum(){
+        return 0;
+    }
+    public int sum(int x, int y){
+        return x+y;
+    }
+    public int sum(int x, int y, int z){
+        return x+y+z;
+    }
+    
+    
+}
